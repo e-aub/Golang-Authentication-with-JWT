3ae4c8a3cd54dc090ca847fc954842d3b55a9544
e-aub <eaub@proton.me>
2024-04-03 02:04:46 +0000

    update

--- main.go
+++ main.go
@@ -16,4 +16,11 @@ func main() {
 	router.Use(gin.Logger())
 	routes.authRoutes(router)
 	routes.userRoutes(router)
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"Success": "Access granted for api-1"})
+	})
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+	router.Run(":" + port)
 }
--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func authRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controller.)
+	incomingRoutes.POST("/users/login", controller.)
+}
